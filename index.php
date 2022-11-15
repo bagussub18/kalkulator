f47c42304e876d9a899d73c2b11376a311de292c
bagussub18 <bagussub18@gmail.com>
2022-11-16 01:16:06 +0700

    edit hasil

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<h1>Kalkulator</h1>
-	<form method = "post" action = "hasil.php">
+	<form method = "post" action ="/git/kalkulator/hasil">
 	<table>
 		<tr>
 			<td><input type = "number" name = "number1"></td>
@@ -16,7 +16,7 @@
 			<td><input type = "number" name = "number2"></td>
 		</tr>
 		<tr>
-			<td><input type = "submit" value = "Jumlah"></td>
+			<td><input type = "submit" value = "hitung"></td>
 		</tr>
 	</table>
 </body>
